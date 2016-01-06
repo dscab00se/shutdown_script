0856c2bcdf9a365c1704df7d2c16f94f21ca1952
Dillon Soma <dillon.soma@gmail.com>
2016-01-06 17:57:43 -0600

    Simple Windows Shutdown Script
    A Simple Windows Shutdown Script used to shut down my girlfriends media
    center PC for when she wants to fall asleep watching Netflix or
    something from our Mini-DLNA server

--- ShutdownScript.py
+++ ShutdownScript.py
@@ -2,28 +2,37 @@
 import time
 import os
 
-
-shutdownTime = input ("In how many minutes would you like to shut down the PC?  ")
+#ask's user for how many hours they would like the computer to continue to run
+hour_Shutdown = input ("In how many hours would you like to shut down the PC?  ")
 time.sleep (.5)
 print()
 print()
 
-shutdownSeconds = int(shutdownTime) * 60
+#the following takes the input from hour_Shut_Down and converts it to seconds
+#for Windows CMD to take as an argument
+shutdown_Sec = (float(hour_Shutdown) * 60) * 60
 
-print ("The computer will shut dowin in " + str(shutdownTime) + " minutes")
+print ("The computer will shut dowin in " + str(hour_Shutdown) + " hours")
 print()
 print()
 
-systemDown = ('shutdown -f -s -t %d' % int(shutdownSeconds))
-cancelDown = ('shutdown -a')
+#the following 2 variables pass shutdown information to windows CMD
+#using the shutdown_Sec var to determine the time of shutdown or to pass
+#the cancel shutdown argument in CMD when asked
+system_Down = ('shutdown -f -s -t %d' % int(shutdown_Sec))
+cancel_Down = ('shutdown -a')
 
-os.system(systemDown)
+#telling windows to shutdown using the above function or to cancel upon having
+#cancel typed by user.
+os.system(system_Down)
 print("Type \'cancel\' to stop shutdown or enter to continue")
 
+#wating for user input
 stop = input()
 
+#if statment to have the computer either shutdown or cancel the shutdown process
 if stop == str('cancel'):
-    os.system(cancelDown)
+    os.system(cancel_Down)
     print()
     input('Press any key to contine...')
 else:
